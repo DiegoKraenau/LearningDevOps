d15dac00570ef6e6e1dc14ff73447433d8c3ff79
KraenauDiegoDoApps <diego.kraeneau@doapps.me>
2021-09-06 01:27:41 -0500

    Add docker compose with environments

---
+++ volumen-orquestation/basedata/init_prod.sql
@@ -0,0 +1,14 @@
+CREATE USER diego WITH PASSWORD 'diego2009';
+DROP DATABASE IF EXISTS diegobd_test;
+CREATE DATABASE diegobd_test;
+GRANT ALL PRIVILEGES ON DATABASE diegobd_test TO diego;
+
+\c diegobd_test;
+CREATE TABLE  users (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR (250)
+);
+
+GRANT ALL PRIVILEGES ON TABLE users TO diego;
+
+INSERT INTO users (id,name) values(1,'Producción');
